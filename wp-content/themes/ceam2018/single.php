4059e7593e45ebdf0f6b3a15fff0f1c1d00b99fe
Brownerd <benjamin@browmerd.com>
2018-08-22 13:11:04 -0500

    updated single blogs

--- wp-content/themes/ceam2018/single.php
+++ wp-content/themes/ceam2018/single.php
@@ -1,23 +1,16 @@
 <?php get_header(); ?>
 
-<?php if (get_full_image_src()) { ?>
-<!--feature section-->
-  <div class="feature__layer" style="background-image: url(<?php echo get_full_image_src(); ?>);"></div>
-<?php } else {?>
 
-  <div class="feature__layer" style="background-image: url(/wp-content/uploads/2018/07/child-02.jpg);"></div>
-<?php } ?>
-  
 <div class="page">
 	<div class="page__container">
     <div class="page__content-padding">
-      <h1 class="post__heading"><?php the_title( ); ?></h1>
-
-      <?php the_content(); ?>
-
+      <h2 class="content__heading"><?php the_title()?></h2>
+      <br>
+      <div class="feature__layer" style="background-image: url(<?php echo get_full_image_src(); ?>);"></div>
+      <?php the_content() ?>
     </div>
   </div>
 </div>
-
+  
 
 <?php get_footer(); ?>
